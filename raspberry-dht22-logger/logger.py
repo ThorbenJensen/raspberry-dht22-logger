8acf32c4c5a69fa5b9621d91a28df9fbe203bea7
Thorben Jensen <jensen.thorben@gmail.com>
2017-11-16 19:21:20 +0100

    added logging script and header of logging file.

---
+++ raspberry-dht22-logger/logger.py
@@ -0,0 +1,19 @@
+# LOG TO CSV
+
+from . import sensor
+
+
+def log():
+    humidity, temperature = sensor.read_sensor()
+
+    if humidity is not None and temperature is not None:
+        print('{0:0.1f},{1:0.1f}'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+        print('Humidity: ' + str(humidity))
+        print('Temperature: ' + str(temperature))
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        log()
