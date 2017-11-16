50b955b018596a446883d785e93c276858a4fe36
Thorben Jensen <jensen.thorben@gmail.com>
2017-11-16 19:58:57 +0100

    adding timestamp to log.

--- raspberry-dht22-logger/logger.py
+++ raspberry-dht22-logger/logger.py
@@ -1,15 +1,24 @@
 # LOG TO CSV
 
 import sensor
+import datetime
 
 
 def log(file_path='../log/log.csv', verbose=True):
+    # get timestamp
+    timestamp = datetime.datetime.now()
+    # read sensor
     humidity, temperature = sensor.read_sensor()
     if verbose:
         print('{0:0.1f},{1:0.1f}'.format(temperature, humidity))
     # write to file
     with open(file_path, 'a') as myfile:
-        myfile.write(str(humidity) + ',' + str(temperature) + '\n')
+        myfile.write(timestamp
+                     + ','
+                     + str(humidity)
+                     + ','
+                     + str(temperature)
+                     + '\n')
 
 
 if __name__ == "__main__":
